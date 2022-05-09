3a5c6b65294fef571d914a464db26f968a9eef3a
Sushilkana <86962755+Sushilkana@users.noreply.github.com>
2022-05-09 14:52:38 +0530

    Add files via upload

--- Bed_room.js
+++ Bed_room.js
@@ -1,5 +1,5 @@
 var img = "";
-var status = false;
+var status = "";
 var object = [];
 
 function preload(){
@@ -15,11 +15,28 @@ function setup(){
 
 function draw(){
 	image(img,0,0,700,400);
+
+		r = random(255);
+        g = random(255);
+        b = random(255);
+        Object_Detector.detect(video, gotresult);
+        for (i = 0; i <= object.length; i++) {
+            document.getElementById("Status").innerHTML = "Status: Detected Objects";
+            document.getElementById("nameofobject").innerHTML = object.lenght;
+
+            fill(r,g,b);
+            noFill();
+            stroke(r,g,b);
+
+            percent = floor(object[i].confidence * 100);
+            text(object[i].lable + " " + percent + "%", object[i].x, object[i].y);
+
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
 }
 
 function modelLoaded(){
 	console.log("The cocossd madel is loaded...");
-	status = true;
+	status = "true";
 	document.getElementById("Status").innerHTML = "Status: Object Detecting...";
 	Object_Detector.detect(image.gotresult);
 }
